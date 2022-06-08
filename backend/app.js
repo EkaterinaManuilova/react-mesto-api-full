1fbae123f3591e1e0e846005b25c896466ae2384
EkaterinaManuilova <e.manuilova@autosyst.ru>
2022-06-08 12:28:28 +0300

    cors заголовки ответа изменены на *

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
   }
 
